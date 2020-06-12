c1db6c21c184a51d2cd47377bdd2569e95d27e69
Stefan Huber <mail@stefanhuber.at>
2020-06-12 11:23:42 +0200

    critical rendering path

---
+++ docs/critical-rendering-path/timing.js
@@ -0,0 +1,10 @@
+function measureCRP() {
+    let span = document.getElementsByTagName('span')[0];
+    span.textContent = 'interactive Example'; 
+    span.style.display = 'inline';  
+    
+    let loadTime = document.createElement('div');
+    loadTime.textContent = 'You loaded this page on: ' + new Date();
+    loadTime.style.color = 'blue';
+    document.body.appendChild(loadTime);
+}
