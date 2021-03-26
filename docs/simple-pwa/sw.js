1090cc1c6756d4cdb6ef249ad3f9dca0361209b8
Stefan Huber <mail@stefanhuber.at>
2021-03-26 12:52:22 +0100

    sw update

--- docs/simple-pwa/sw.js
+++ docs/simple-pwa/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('simple-pwa-v1').then(function(cache) {
-            return cache.addAll([                
+        caches.open('simple-pwa-v1')
+            .then(cache => cache.addAll([                
                 'index.html',
                 'main.js',
                 'style.css',
@@ -9,9 +9,8 @@ self.addEventListener('install', function(event) {
                 'icon192.png',
                 'icon196.png',
                 'icon512.png',
-                'manifest.webiste'
-            ])
-        })
+                'manifest.webmanifest'
+            ]))
     );
 });
 
