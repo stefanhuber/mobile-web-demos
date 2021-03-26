71f38ad715bfb8da0aa6a7578405eb2f75645b36
Stefan Huber <mail@stefanhuber.at>
2021-03-26 12:27:34 +0100

    fix simple pwa

--- docs/simple-pwa/sw.js
+++ docs/simple-pwa/sw.js
@@ -2,12 +2,12 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('simple-pwa-v1').then(function(cache) {
             return cache.addAll([
-                '/simple-pwa/',
-                '/simple-pwa/index.html',
-                '/simple-pwa/main.js',
-                '/simple-pwa/style.css',
-                '/simple-pwa/icon16.png',
-                '/simple-pwa/icon512.png'
+                '/mobile-web-demos/simple-pwa/',
+                '/mobile-web-demos/simple-pwa/index.html',
+                '/mobile-web-demos/simple-pwa/simple-pwa/main.js',
+                '/mobile-web-demos/simple-pwa/simple-pwa/style.css',
+                '/mobile-web-demos/simple-pwa/simple-pwa/icon16.png',
+                '/mobile-web-demos/simple-pwa/simple-pwa/icon512.png'
             ])
         })
     );
