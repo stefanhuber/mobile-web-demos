eed93fcc3283bc3372d4efa447b2c41c8e11bae9
Stefan Huber <mail@stefanhuber.at>
2021-04-22 12:24:18 +0200

    add PWA install prompt

--- docs/simple-pwa/main.js
+++ docs/simple-pwa/main.js
@@ -1,11 +1,53 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('button');
+const button = document.querySelector('main > button');
+const prompt = document.querySelector('article');
 
-    button.addEventListener('click', () => {
-        let colorChannels = [];
-        for (let i = 0; i < 3; i++) {
-            colorChannels.push(Math.floor(Math.random() * 256));
-        }
-        document.querySelector('body').style.background = "rgb(" + colorChannels.join(', ') + ")";
-    });
+function updatePWADisplayMode() {
+    let displayMode = "browser";
+
+    const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
+    if (document.referrer.startsWith('android-app://')) {
+        displayMode = 'twa'; // Trusted Web Activity
+    } else if (navigator.standalone || isStandalone) { // navigator.standalone iOS hack
+        displayMode = 'standalone';
+    }
+
+    document.querySelector('h1').textContent = `Simple PWA [${displayMode}]`;    
+}
+
+button.addEventListener('click', () => {
+    let colorChannels = [];
+    for (let i = 0; i < 3; i++) {
+        colorChannels.push(Math.floor(Math.random() * 256));
+    }
+    document.querySelector('body').style.background = "rgb(" + colorChannels.join(', ') + ")";
 });
+
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    prompt.style['display'] = 'block';
+});
+
+window.addEventListener('appinstalled', () => {    
+    prompt.style['display'] = 'none';
+    deferredPrompt = null;
+    updatePWADisplayMode();
+});
+
+prompt.addEventListener('click', function(event) {
+    if (event.target.dataset.id == 'install-yes' && deferredPrompt) {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then(result => {
+            console.log("result of user prompt", result);
+            prompt.style['display'] = 'none';
+            deferredPrompt = null;
+            updatePWADisplayMode();
+        });        
+    } else {
+        updatePWADisplayMode();
+        prompt.style['display'] = 'none';
+    }
+});
+
+updatePWADisplayMode();
--- docs/simple-pwa/sw.js
+++ docs/simple-pwa/sw.js
@@ -2,14 +2,15 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('simple-pwa-v1')
             .then(cache => cache.addAll([                
-                'index.html',
-                'main.js',
-                'style.css',
-                'icon16.png',
-                'icon192.png',
-                'icon196.png',
-                'icon512.png',
-                'manifest.webmanifest'
+                './',
+                './index.html',
+                './main.js',
+                './style.css',
+                './icon16.png',
+                './icon192.png',
+                './icon196.png',
+                './icon512.png',
+                './manifest.webmanifest'
             ]))
     );
 });
